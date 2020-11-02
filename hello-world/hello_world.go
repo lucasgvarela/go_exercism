072e9fa4a57ff6006d2ede31b3326b83365f4563
Lucas Grigolon Varela <lucasgvarela@pm.me>
2020-11-02 16:04:38 -0300

    first commit

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+package greeting
+
+// Will return "Hello, World!"
+func HelloWorld() string {
+
+	return "Hello, World!"
+}
---
+++ two-fer/go.mod
@@ -0,0 +1,5 @@
+module twofer
+
+go 1.13
+
+require golang.org/x/tools v0.0.0-20201102043006-b53d4cbd60a6 // indirect
